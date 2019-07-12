873b348f2ff542090e13a1eb5d68debbdf55a29a
nae.yamamoto <yamamotonae333@gmail.com>
2019-07-12 23:44:05 +0900

    Add DestroyServlet.java

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -14,16 +13,16 @@ import models.Task;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class UpdateServlet
+ * Servlet implementation class DestroyServlet
  */
-@WebServlet("/update")
-public class UpdateServlet extends HttpServlet {
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UpdateServlet() {
+    public DestroyServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,23 +31,16 @@ public class UpdateServlet extends HttpServlet {
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String _token =(String)request.getParameter("_token");
+        String _token = (String)request.getParameter("_token");
         if(_token != null && _token.equals(request.getSession().getId())){
             EntityManager em = DBUtil.createEntityManager();
 
-            //セッションスコープからメッセージのIDを取得して
-            //該当のIDのメッセージ1件のみをデータベースから取得
-            Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
+            //セッションスコープからメッセージIDを取得して
+            //該当IDのメッセージ1件のみをデータベースから取得
+            Task t = em.find(Task.class,  (Integer)(request.getSession().getAttribute("task_id")));
 
-            //フォームの内容を各フィールドに上書き
-            String content = request.getParameter("content");
-            t.setContent(content);
-
-            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            t.setUpdated_at(currentTime);    //更新日時のみ上書き
-
-            //データベースを更新
             em.getTransaction().begin();
+            em.remove(t);
             em.getTransaction().commit();
             em.close();
 
